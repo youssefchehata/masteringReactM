df3f7b85afdcbac7745d3451c4ebfd8788fab2fb
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-03-16 22:26:14 +0100

    grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,45 +1,20 @@
 import React from "react";
 const Grid = () => {
   return (
-  <div classNameName="row">
-
-  <div className="col-sm-2">
-    <div className="callout callout-info b-t-1 b-r-1 b-b-1">
-      <small className="text-muted">New Clients</small><hr/>
-      <strong className="h4">9,123</strong>
-    </div>
+  <div className="container">
+  <div className="row d-flex justify-content-start border">
+  <div className="bg-success col-md-6 d-flex justify-content-end">col</div>
+  <hr/>
+  <div className="bg-warning col-md-4 d-flex justify-content-start">vol</div>
+  <hr/>
+  <div className="bg-danger col-md-4 d-flex justify-content-center">col</div>
+  <hr/>
+  <div className="bg-primary col-md-4 d-flex justify-content-end">col</div>
+  <hr/>
+  <div className="bg-info col-md-12 d-flex justify-content-end">col</div>
   </div>
-  <div className="col-sm-2">
-    <div className="callout callout-danger b-t-1 b-r-1 b-b-1">
-      <small className="text-muted">Recuring Clients</small><hr/>
-      <strong className="h4">22,643</strong>
-    </div>
+ 
   </div>
-  <div className="col-sm-2">
-    <div className="callout callout-warning b-t-1 b-r-1 b-b-1">
-      <small className="text-muted">Pageviews</small><hr/>
-      <strong className="h4">78,623</strong>
-    </div>
-  </div>
-  <div className="col-sm-2">
-    <div className="callout callout-success b-t-1 b-r-1 b-b-1">
-      <small className="text-muted">Organic</small><hr/>
-      <strong className="h4">49,123</strong>
-    </div>
-  </div>
-  <div className="col-sm-2">
-    <div className="callout b-t-1 b-r-1 b-b-1">
-      <small className="text-muted">CTR</small><hr/>
-      <strong className="h4">23%</strong>
-    </div>
-  </div>
-  <div className="col-sm-2">
-    <div className="callout callout-primary b-t-1 b-r-1 b-b-1">
-      <small className="text-muted">Bounce Rate</small><hr/>
-      <strong className="h4">5%</strong>
-    </div>
-  </div>
-</div>
   );
 };
 
