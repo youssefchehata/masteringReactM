9bc905dccb94fa4fe7a2ffa97d634cf5087434b4
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-04-26 16:58:34 +0100

    info

--- src/a_ticketing/edit&deleteModel/editModal/comp1.jsx
+++ src/a_ticketing/edit&deleteModel/editModal/comp1.jsx
@@ -4,70 +4,108 @@ import LoadingOverlay from "react-loading-overlay";
 import Swal from 'sweetalert2'
 // import InfoUserEditModels from "../../../../../../models/User/InfoUserEditModels"
 import BankService from "../../../../../../services/bank.service";
-import BankEditModel from '../../../../../../models/Bank/BankEditModels';
+
 
 
 class Info extends Component {
   state = {
 
-      model: new BankEditModel(0, ""),
-      submitted: false,
-      error: false,
-      loading: true
-    };
-    bankService = new BankService()
-  
+    model: {},
+    submitted: false,
+    error: false,
+    loading: true,
 
 
-    async componentDidMount() {
-      try {
+  };
+  bankService = new BankService()
+
+
+  async componentDidMount() {
+    try {
       const { data: model } = await this.bankService.get(this.props.id)
-    
-      this.setState({ model , loading: false })
+
+      this.setState({ model, loading: false });
+
     } catch (ex) { alert("An unexpected error occurred.") }
+
+  }
+
+  handleChange = e => { 
+    let model =this.state.model
+    model[e.target.name]=e.target.value
+    this.setState({  model })
+   };
   
+  // -----------submit-------------
+  handleSubmit = (e) => {
+
+    e.preventDefault();
+    this.setState({ submitted: true });
+    if (this.state.model.label) {
+      this.bankService
+        .put(this.state.model)
+
+        .then((response) => {
+          alert("submitted")
+
+        })
+        .catch(error => {
+          this.setState({ error: true });
+          alert(
+            'error'
+          )
+        });
     }
-  // fetchData = (id) => {
-  //   this.setState({ loading: true })
-  //   if (id) {
-  //     this.bankService.get(id)
-  //       .then((response) => {
-  //         const model = new BankEditModel(
-  //           response.data.id, (response.data.label) ? response.data.label : "");
-  //         console.log(model);
-  //         this.setState({ model: model, loading: false });
-  //       })
-  //       .catch(error => { alert("error"); }
-  //       );
-  //   }
-  // }
-
-  // componentDidMount = () => this.fetchData(this.props.id)
-
+  }
 
   render() {
-
-    const { onChange, onClick } = this.props;
+    // console.log(this.state.model);
+    const { onClick } = this.props;
     const { model } = this.state;
-    const label = model.label;
-    console.log(label)
+
+
     return (
 
       <div className="container" >
         <LoadingOverlay text="Loading ..." active={this.state.loading} spinner={<Spinner name="ball-clip-rotate" color="white" />} >
 
-        <div className="row">
-          <div className="col">
-            <div className="form-group">
-              <label>Banque <span style={{ color: "red" }}>*</span></label>
-              <input type="text" className="form-control" name="label" value={label} onChange={this.handleChange} />
-            </div>
-           
-            <div className="form-group">
-              <button type="button" className="btn btn-custom-yellow" onClick={this.handleSubmit} > Enregistrer </button>
+          <div className="row">
+            <div className="col">
+              <form  submitted={this.state.submitted}>
+                <div className="form-group">
+                  <label>Nom d'utilisateur</label>
+                  <input type="text" className="form-control" name="label" value={model.label} onChange={this.handleChange} />
+                </div>
+              </form>
+              <div className="form-group">
+                <label>Nom</label>
+                <input type="text" className="form-control" name="code" value={model.code} onChange={this.handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Prénom</label>
+                <input type="text" className="form-control" name="label" value={model.label} onChange={this.handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Téléphone</label>
+                <input type="text" className="form-control" name="label" value={model.label} onChange={this.handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Email</label>
+                <input type="email" className="form-control" name="label" value={model.label} onChange={this.handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Position</label>
+                <input type="text" className="form-control" name="label" value={model.label} onChange={this.handleChange} />
+              </div>
+
+              <div className="form-group">
+
+                <button type="button" className="btn btn-custom-yellow" onClick={this.handleSubmit} > Enregistrer </button>
+
+              </div>
             </div>
           </div>
-        </div>
+
         </LoadingOverlay>
       </div>
 
