c8062f1a64bd412ec118db46c846bad565b3a722
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-06-06 02:45:54 +0100

    traks

--- src/components/tracks/track.js
+++ src/components/tracks/track.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react'
 class Track extends Component {
     state = {}
     render() { 
+        const {track}=this.props
         return ( 
-            <div className="container">
-                Track-:p
-            </div>
+            <div className="col-md-6">
+      <div className="card mb-4 shadow-sm">
+        <div className="card-body">
+          <h5>{track.artist_name}</h5>
+          {/* <p className="card-text">
+            <strong> <i className="fas fa-play" /> Track </strong>
+            : {track.track_name}
+            <br />
+            <strong>
+              <i className="fas fa-compact-disc" /> Album
+            </strong>
+            : {track.album_name}
+          </p> */}
+          {/* <Link
+            to={`lyrics/track/${track.track_id}`}
+            className="btn btn-dark btn-block"
+          >
+            <i className="fas fa-chevron-right" /> View Lyrics
+          </Link> */}
+        </div>
+      </div>
+    </div>
          )
     }
 }
--- src/components/tracks/tracks.js
+++ src/components/tracks/tracks.js
@@ -3,20 +3,48 @@ import Track from './track';
 import Search from './search';
 import Spinner from './layout/Spinner';
 import Navbar from './layout/Navbar';
+import axios from 'axios';
 class Tracks extends Component {
   state = {
-    track_list: 'j',
-    heading: '5'
+    track_list: [
+        {track:{artist_name:'abc'}},
+        {track:{artist_name:'123'}},
+    ],
+    heading: 'Top 10 Tracks'
   };
+
+  componentDidMount = () => {
+    axios
+      .get(
+        ` https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=02b394b42e0e97a4372d9754855b7d37`
+        
+        )
+      .then(res => {
+        console.log(res.data)
+        // this.setState({ track_list: res.data.message.body.track });
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
-    if (this.state.track_list.length === 0 || this.state.heading.length === 0)
-      return <Spinner />;
+      console.log(this.state.track_list.track);
+    const { track_list, heading } = this.state;
+
+    if (track_list.length === 0 || track_list === undefined) return <Spinner />;
     else
       return (
-        <div className="container">
-          <Navbar />
+        <div className="">
+          {/* <Navbar /> */}
           <Search />
-          <Track />
+          <h3 className="text-center">{heading}</h3>
+          {/* {track_list.map(item => (
+            <Track key={item.track.track_id} track={item.track} />
+          ))} */}
+          <div className="text-center">
+            {this.state.track_list.map(item => ( <li key={item.id}> {item.track.artist_name}  </li> ))}
+          </div>
+           
+        
         </div>
       );
   }
