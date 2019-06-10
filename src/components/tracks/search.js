308ffde183276f6f0f0f2c248720646e66b5dabd
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-06-10 15:41:23 +0100

    jj

--- src/components/tracks/search.js
+++ src/components/tracks/search.js
@@ -1,48 +1,56 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 class Search extends Component {
-  state = {
-    trackTitle:''
-  };
-  handleChange = (e) =>{
-    this.setState({ [e.target.name] : e.target.value})
+  constructor(props) {
+    super(props);
+    this.state = {
+      trackTitle: '',
+      
+    };
   }
-  handleSubmit=(e)=>{
-e.preventDefault()
-  }
-  //   useEffect(() => {
-  //   axios
-  //     .get(
-  //       `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${
-  //         process.env.REACT_APP_MM_KEY
-  //       }`
-  //     )
-  //     .then(res => {
-  //       let track_list = res.data.message.body.track_list;
-  //       setState({ track_list: track_list, heading: "Search Results" });
-  //     })
-  //     .catch(err => console.log(err));
-  // }, [trackTitle]);
+ 
 
-  // const findTrack = e => {
-  //   e.preventDefault();
-  //   setTrackTitle(userInput);
-  // };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    const { trackTitle } = this.state;
+    let url = `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=02b394b42e0e97a4372d9754855b7d37`;
+    e.preventDefault();
+    axios
+      .get(url)
+      .then(res => {
+        // console.log(res.data);
+        this.props.setfilter(res.data.message.body.track_list);
+      })
+      .catch(err => { console.log(err); });
+  };
+
   render() {
-    const {trackTitle}=this.state
+    console.log(this.state.track_list);
+
     return (
       <div className="container">
-          <h1 className="display-4 text-center">
-        <i className="fas fa-music" /> Search For A Song
-      </h1>
-      <p className="lead text-center">Get the lyrics for any song</p>
-        <form onSubmit={this.handleSubmit} >
-          <input type="text" className="form-control form-control-lg" 
-          placeholder="Search..."name={trackTitle} handleChange={this.handleChange} />
-          <button className="btn btn-primary btn-lg btn-block my-4" type="submit">
-          Get Track Lyrics
-        </button>
+        <h1 className="display-4 text-center">
+          <i className="fas fa-music" /> Search For A Song
+        </h1>
+        <p className="lead text-center">Get the lyrics for any song</p>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            value      ={this.state.trackTitle}
+            placeholder="Search..."
+            name       ="trackTitle"
+            onChange   ={this.handleChange}
+          />
+          <button
+            className="btn btn-primary btn-lg btn-block my-4"
+            type="submit"
+          >
+            Get Track Lyrics
+          </button>
         </form>
       </div>
     );
--- src/components/tracks/tracks.js
+++ src/components/tracks/tracks.js
@@ -7,18 +7,18 @@ import axios from 'axios';
 class Tracks extends Component {
   state = {
     track_list: [
-        // {track:{track_id:1, artist_name:'abc'}},
-        // {track:{track_id:2, artist_name:'123'}},
+      // {track:{track_id:1, artist_name:'abc'}},
+      // {track:{track_id:2, artist_name:'123'}},
     ],
     heading: 'Top 10 Tracks'
   };
+  setfilter = (filterlist) => { this.setState({ track_list: filterlist }); };
 
   componentDidMount = () => {
     axios
       .get(
         `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=02b394b42e0e97a4372d9754855b7d37`
-        
-        )
+      )
       .then(res => {
         // console.log(res.data)
         this.setState({ track_list: res.data.message.body.track_list });
@@ -27,28 +27,26 @@ class Tracks extends Component {
   };
 
   render() {
-      console.log(this.state.track_list.track);
+    console.log(this.state.track_list.track);
     const { track_list, heading } = this.state;
 
     if (track_list.length === 0 || track_list === undefined) return <Spinner />;
     else
       return (
         <React.Fragment>
-           <Navbar />
-        <div className="container">
-        
-          <Search />
-          <h3 className="text-center mb-5">{heading}</h3>
-          <div className="row  justify-content-center ">
-          {track_list.map(item => (
-            <Track key={item.track.track_id} track={item.track} />
-          ))}
-        </div>
-        
-        {/* {this.state.track_list.map(item => ( <li key={item.id}> {item.track.artist_name}  </li> ))} */}
-        </div>
+          <Navbar />
+          <div className="container">
+            <Search setfilter={this.setfilter} />
+            <h3 className="text-center mb-5">{heading}</h3>
+            <div className="row  justify-content-center ">
+              {track_list.map(item => (
+                <Track key={item.track.track_id} track={item.track} />
+              ))}
+            </div>
+
+            {/* {this.state.track_list.map(item => ( <li key={item.id}> {item.track.artist_name}  </li> ))} */}
+          </div>
         </React.Fragment>
-         
       );
   }
 }
