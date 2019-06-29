cfd521dd1d43d70d57436edc94a4195e7324bb21
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-06-29 13:20:50 +0100

    predeploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-
+  </HashRouter>,
+// BrowserRouter changed by HashRouter
   document.getElementById('root')
 );
