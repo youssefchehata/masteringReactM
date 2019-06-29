8e6d83f5fe652ea3be5f9d8e473ef2578113f49c
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-06-29 02:26:39 +0100

    Updates

---
+++ precache-manifest.935386f5ed594f64d8268c17b689e330.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e16507effb4916c231a338e89adfa288",
+    "url": "/youssefchehata/masteringReactM/static/media/bg.e16507ef.svg"
+  },
+  {
+    "revision": "7701c814c4bb7b8c8eea",
+    "url": "/youssefchehata/masteringReactM/static/css/main.9b2bfe0e.chunk.css"
+  },
+  {
+    "revision": "e6c9398107bb8384a23b",
+    "url": "/youssefchehata/masteringReactM/static/js/runtime~main.e6c93981.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/youssefchehata/masteringReactM/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "aa352feba42d2e1ae7e4",
+    "url": "/youssefchehata/masteringReactM/static/js/2.aa352feb.chunk.js"
+  },
+  {
+    "revision": "1e2ae4e7a1ba0a9e9a0dfcd6ab64df4e",
+    "url": "/youssefchehata/masteringReactM/static/media/logo.1e2ae4e7.svg"
+  },
+  {
+    "revision": "7406fcfd453bdf2bc435b1c85ce086ef",
+    "url": "/youssefchehata/masteringReactM/static/media/one.7406fcfd.jpeg"
+  },
+  {
+    "revision": "7701c814c4bb7b8c8eea",
+    "url": "/youssefchehata/masteringReactM/static/js/main.7701c814.chunk.js"
+  },
+  {
+    "revision": "ea505863d80247548cbb4be3e4ea1075",
+    "url": "/youssefchehata/masteringReactM/static/media/logo.ea505863.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/youssefchehata/masteringReactM/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/youssefchehata/masteringReactM/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/youssefchehata/masteringReactM/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/youssefchehata/masteringReactM/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "aa352feba42d2e1ae7e4",
+    "url": "/youssefchehata/masteringReactM/static/css/2.feffee83.chunk.css"
+  },
+  {
+    "revision": "6bcda75c20d1c202b6b659afef82e8b1",
+    "url": "/youssefchehata/masteringReactM/index.html"
+  }
+];
