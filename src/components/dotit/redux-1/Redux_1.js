89a6e45d1365e381291d4866441cdf8fd187459b
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-01-05 19:37:05 +0100

    save

--- src/components/dotit/redux-1/Redux_1.js
+++ src/components/dotit/redux-1/Redux_1.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import SongList from './song/SongList';
 import SongDetails from './song/SongDetails';
 import TrendingHotel from './userId/TrendingHotel';
 class Redux_1 extends Component {
-    state = {}
-    render() { 
-        return ( <div className="container pt-4">
-            <div className="row">
-                <div className="col-6"><SongList/></div>
-                <div className="col-6"><SongDetails/></div>
-            </div>
-            <div><TrendingHotel/> </div>
-             </div> )
-    }
+
+  render() {
+    return (
+      <div className='container pt-4'>
+        <div className='row'>
+          <div className='col-6'>
+            <SongList />
+          </div>
+          <div className='col-6'>
+            <SongDetails />
+          </div>
+        </div>
+        <div
+          className='coverflow-auto'
+          style={{
+            overflowY: 'scroll',
+            border: '1px solid ',
+            width: '500px',
+            
+            height: '500px',
+            // position: 'relative'
+          }}
+        >
+          <TrendingHotel />
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Redux_1;
