8822a7c7dd07db1fd09d57b9f052a217eca8ea5d
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2020-01-24 09:50:28 +0100

    save

--- src/components/dotit/redux-1/HOTELS&FILTER/hotelsLists.js
+++ src/components/dotit/redux-1/HOTELS&FILTER/hotelsLists.js
@@ -16,51 +16,29 @@ gethotel=()=>{
    return(<div className="card m-3 " style={{width: "18rem",
    boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
 <div className="shadow-lg">
-
-    {el.photo_produit.slice(0,1).map(sub=>{
-    return <img className="card-img-top" src={this.state.url+sub.file_name}
-    alt=""/>
-  })}
-
-
-
-
+    {el.photo_produit.slice(0,1).map(sub=>{ return <img className="card-img-top" src={this.state.url+sub.file_name} alt=""/> })}
 </div>
 <div className="card-body  text-center">
-
 <h5>categorie:{el.categorie}</h5>
-    <h4 className="card-title"><strong><Link to="">{el.libelle_hotel}</Link></strong></h4>
-
+  <h4 className="card-title"><strong><Link to="">{el.libelle_hotel}</Link></strong></h4>
 <h1 className="rating  d-flex justify-content-center " style={{listStyleType: 'none',color:'#FFD700'}}>
-
 {/* <Rating score={el.average_rating}/> */}
-
 </h1>
-
 <div className="card-text">{el.adresse}
 </div>
-
 <div className="card-footer">
 <span className="float-left">49$</span>
 <span className="float-right">
-
 <Link to='' className="active" data-toggle="tooltip" data-placement="top" title="Added to Wishlist"><i className="fas fa-heart"></i></Link>
-</span>
-</div>
-
-</div>
-</div>
+</span> </div> </div> </div>
    ) 
     })
   }
     render() { 
         return <div className='container-fluid ' style={{ backgroundColor: '#EDEDED' }}>
-
         <div className=' row justify-content-center  '>
           <div className="col-sm-12"><FilterByDropDown/></div>
-          <div className="col-sm-12">{this.gethotel()}</div>
-        
-        
+          <div className="col-sm-12">{this.gethotel()}</div>   
         </div>
       </div>
     }
